bb516bf558a124c5e3364486b20c59a9d67b2eaf
Mads Peter Rommedahl <madspeter@rommedahl.info>
2025-03-15 08:16:12 +0100

    before home manager

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -12,6 +8,30 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mpr = {
+    isNormalUser = true;
+    description = "Mads Peter Rommedahl";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      brave
+      discord
+      fractal
+      keepassxc
+      mpv
+      obsidian
+      vscode
+    ];
+  };
+  
+  environment.systemPackages = with pkgs; [
+    git
+    gnome-tweaks
+    gnomeExtensions.appindicator
+    neovim
+    pciutils
+    wget
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -48,11 +68,51 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Disable Nouveau
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  boot.kernelParams = [ "nomodeset" "modprobe.blacklist=nouveau" ];
+
+  
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+
   environment.variables.GNOME_SHELL_EXTENSIONS = "appindicator";
-   # Add custom mpv configuration
-  environment.etc."mpv/mpv.conf".text = ''
-    loop-file=inf
-  '';
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -79,18 +139,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.mpr = {
-    isNormalUser = true;
-    description = "Mads Peter Rommedahl";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      brave
-      keepassxc
-      mpv
-      vscode
-    ];
-  };
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
@@ -103,16 +151,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    gnome-tweaks
-    gnomeExtensions.appindicator
-    neovim
-    pciutils
-    wget
-  ];
 
   services.udev.packages = [ pkgs.gnome-settings-daemon ];
 
